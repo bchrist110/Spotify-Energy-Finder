c64184eda5b723f3ed5848244f7c107f8cb0d3ab
Brandon Christianson <b.christianson110@gmail.com>
2020-10-03 14:40:30 -0700

    10/3

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 'use strict'
 
 // put your own value below!
-const token = 'bnFiDu6xVmAQYHDd0cWSaAqHeEk2DRXUHSrvAAPS'; 
+const token = 'BQDr-eeWiVJ3fz9RVrsC8QUBKWM_xvGaWZYs7kMH0gCfPiByae_z3-Ruw-4D2EmL3KtwrGiW7oK3ytrYQgQu1CV9kvXvwtgP7nOpcIdQzY_E0fl-VBlJKIzfpjg_OVpRmHqjiT8tEbA'; 
 const searchURL = 'https://api.spotify.com/v1';
 
-
-
-const Url = 'https://accounts.spotify.com/api/token'
 
 function post() {
     const data = {grant_type: 'client_credentials'}
     fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
-            "Authorization" : "Basic <ZTYxNjAyZjFiOTI1NGI5NTlhZWZkNDc1OWEwZTQ3MTU6ZGExMjg5OTRhNDk1NGNjYjhjZGJmZjQ2YzNmMTgxOTM=>",
+            "Authorization" : "Basic ZTYxNjAyZjFiOTI1NGI5NTlhZWZkNDc1OWEwZTQ3MTU6ZGExMjg5OTRhNDk1NGNjYjhjZGJmZjQ2YzNmMTgxOTM=",
         },
         body: JSON.stringify(data),
     })
@@ -32,14 +29,14 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function getParks(string, limit=10) {
+function search(string, limit=10, typeOfMusic) {
   const params = {
-    stateCode: string,
+    q: string,
+    type: typeOfMusic,
     limit,
-    api_key: apiKey
   };
   const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+  const url = searchURL +"/search/" + '?' + queryString;
 
   fetch(url)
     .then(response => {
@@ -57,10 +54,12 @@ function getParks(string, limit=10) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    post()
-    //const searchTerm = $('#js-search-term').val();
-    //const maxResults = $('#js-max-results').val();
-    //getParks(searchTerm, maxResults);
+    //post()
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    const typeOf = $('#type').val();
+    
+    //search(searchTerm, maxResults, typeOf);
   });
 }
 
